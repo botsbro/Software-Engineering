3aa084cb030463eec38a9e773a332e409ca2824c
Thomas Mark Botsford <botsbro@users.noreply.github.com>
2020-08-16 22:47:06 +0100

    Add files via upload

---
+++ src/softwareengineeringproject/student.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package softwareengineeringproject;
+
+/**
+ *
+ * @author 23657952
+ */
+public class student {
+    
+    private String name;
+    private String skill;
+    private team team;
+    
+    student(String n, String s, team t){ //overriding constructor takes a string for name, another for skill, and an team object for team
+        this.name = n;
+        this.skill = s;
+        this.team = t;
+        
+    }
+    student(String n, String s){ //constructor takes a string for name, another for skill
+        this.name = n;
+        this.skill = s;
+        
+    }
+    
+    public void setTeam(team t){
+        this.team = t;
+    }
+    
+    public void setName(String n){
+        this.name = n;
+    }
+    
+    public team getTeam(){
+        return this.team;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public String getSkill(){
+        return this.skill;
+    }
+    
+    
+}
---
+++ src/softwareengineeringproject/team.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package softwareengineeringproject;
+
+/**
+ *
+ * @author 23657952
+ */
+public class team {
+    
+    private int teamNumber;
+    public student[] students;
+    
+    team(int num, student[] s){ //constructor takes an int for team number and an array for students
+        this.students = s;
+        
+    }
+    
+    
+}
